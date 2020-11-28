a8a4d064fa5d97267152f352c4474eb801e63279
tommadness <tom.m.cover@gmail.com>
2020-11-28 07:58:01 -0500

    Add project files.

---
+++ KH2TrackAuto/MainWindow.xaml.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Timers;
+using System.ComponentModel;
+
+namespace KH2TrackAuto
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window, INotifyPropertyChanged
+    {
+        private static System.Timers.Timer aTimer;
+
+        private int hjContent;
+        public int HJContent {
+            get { return hjContent; }
+            set { hjContent = value;
+                OnPropertyChanged("HJContent");}
+        }
+        public MainWindow()
+        {
+            InitializeComponent();
+            SetBindings();
+            SetTimer();
+        }
+        private void SetBindings()
+        {
+            Binding HJbinding = new Binding("HJContent");
+            HJbinding.Source = this;
+            HighJump.SetBinding(Label.ContentProperty, HJbinding);
+        }
+        private void SetTimer()
+        {
+            // Create a timer with a two second interval.
+            aTimer = new System.Timers.Timer(500);
+            // Hook up the Elapsed event for the timer. 
+            aTimer.Elapsed += OnTimedEvent;
+            aTimer.AutoReset = true;
+            aTimer.Enabled = true;
+        }
+
+        private void OnTimedEvent(object sender, ElapsedEventArgs e)
+        {
+            HJContent = new MemoryReader(0x2032E0FE, 2).ReadMemory()[0];
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void OnPropertyChanged(string info)
+        {
+            PropertyChangedEventHandler handler = PropertyChanged;
+            if (handler != null)
+            {
+                handler(this, new PropertyChangedEventArgs(info));
+            }
+        }
+    }
+}
+
---
+++ KH2TrackAuto/MemoryReader.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace KH2TrackAuto
+{
+    public class MemoryReader
+    {
+        const int PROCESS_WM_READ = 0x0010;
+
+        [DllImport("kernel32.dll")]
+        public static extern IntPtr OpenProcess(int dwDesiredAccess, bool bInheritHandle, int dwProcessId);
+
+        [DllImport("kernel32.dll")]
+        public static extern bool ReadProcessMemory(int hProcess, int lpBaseAddress, byte[] lpBuffer, int dwSize, ref int lpNumberOfBytesRead);
+
+        Int32 address;
+        int bytesToRead;
+        public MemoryReader(Int32 addr, int bytes)
+        {
+            address = addr;
+            bytesToRead = bytes;
+
+        }
+
+        public byte[] ReadMemory()
+        {
+            Process process = new Process();
+            try
+            {
+                process = Process.GetProcessesByName("pcsx2")[0];
+                IntPtr processHandle = OpenProcess(PROCESS_WM_READ, false, process.Id);
+                int bytesRead = 0;
+                byte[] buffer = new byte[bytesToRead]; //'Hello World!' takes 12*2 bytes because of Unicode 
+
+                // 0x0046A3B8 is the address where I found the string, replace it with what you found
+                ReadProcessMemory((int)processHandle, address, buffer, buffer.Length, ref bytesRead);
+                return buffer;
+            }
+            catch (IndexOutOfRangeException e)
+            {
+                return new byte[0];
+            }
+        }
+    }
+}
